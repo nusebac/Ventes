c8db89cbc8426fc98ceb17deede158ac41b561f9
Arka Gupta <nusebac@users.noreply.github.com>
2015-04-16 13:16:37 +0800

    Create Scrape_Main.py
    Main Script to call

---
+++ Scrape_Main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 14:37:52 2015
+
+@author: HP
+"""
+
+import Times_Scrape_Co
+
+
+Company_Lists = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','Others']
+
+for Company_List in Company_Lists:
+    print "---------------------Scraping :%s------------------------"%(Company_List)
+    Times_Scrape_Co.main_scrap(Company_List)
+    
+
